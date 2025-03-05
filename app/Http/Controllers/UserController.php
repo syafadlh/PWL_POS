d6d4838f4951f633b99361b3a9079103a5d3eacf
= <=>
2025-03-05 11:31:46 +0700

    Praktikum 2.2: Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,9 +23,11 @@ class UserController extends Controller
         // $user = UserModel::findOr(1, ['username', 'nama'], function () {
         //     abort(404);
         // });
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
