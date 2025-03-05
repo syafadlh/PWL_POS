3bed1fce97ca6ff755a45a63e129d116f3c56210
= <=>
2025-03-05 12:02:35 +0700

    Praktikum 2.3: Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,24 +10,8 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::firstWhere('level_id', 1);
-        // $user = UserModel::find(1);
-        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $userCount = UserModel::where('level_id', 2)->count();
+        return view('user', ['userCount' => $userCount]);
     }
+    
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
 <head>
     <title>Data User</title>
@@ -20,6 +20,32 @@
             <td>{{ $data->level_id }}</td>
         </tr>
         @endforeach
+    </table>
+</body>
+</html> --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data User</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table>
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $userCount }}</td>
+        </tr>
     </table>
 </body>
 </html>
