d2ea815649568acf73bf9b73b4e4fef33363e7a9
= <=>
2025-04-16 20:03:45 +0700

    JS07 - Praktikum 2: Implementasi Authorizaton di Laravel dengan Middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,21 +6,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\LevelModel;
-use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'm_user';    
+    protected $table = 'm_user';      
     protected $primaryKey = 'user_id';  
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_ad', 'updated_ad'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_ad', 'updated_ad']; 
     protected $hidden = ['password']; 
     protected $casts = ['password' => 'hashed']; 
     
-    //relasi ke tabel level
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    //Mendapatkan nama role 
+    public function getRoleName(){
+        return $this->level->level_nama;
+    }
+
+    //Cek apakah user memeliki role tertentu
+    public function hasRole($role){
+        return $this->level->level_id == $role;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,7 @@ Route::middleware(['auth'])->group(function () { //artinya semua route di dalam 
         Route::delete('/{id}', [UserController::class, 'destroy']); // Hapus user
     });
 
-    Route::group(['prefix' => 'level'], function () {
+    Route::middleware(['authorize:ADM']) -> group (function() {
         Route::get('/', [LevelController::class, 'index']);
         Route::post("/list", [LevelController::class, 'list']);
         Route::get('/create', [LevelController::class, 'create']);
